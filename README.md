f53fe8a059c6388d2b45909b6c39efba3300533a
sophialloydgeorge <slloydgeorge@oapb.org>
2018-07-10 14:07:19 +0000

    automatically backed up by learn

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
-# Dance Instructions Lab
-
+# Dance Instructions Labd
+def Gangnam Style 
+puts "Plant legs far apart, bend knees slightly and keep posture loose."
+puts "Lift right foot."
+puts "Return right foot to the ground."
+puts "Finishing with a small skip-step backward."
+puts "Lower left foot to the ground."
+puts "Bounce left foot back up slightly, kicking it a few inches back."
+puts "Left, Right, Left and Left."
+puts ""
 ## Objectives
 
 1. Create methods that print the steps for different dances moves using `def`
--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,62 @@
+def starting_stance
+  puts "Plant legs far apart" 
+end 
+def base_footwork
+  puts "Lift right foot."
+  puts "Return right foot to the ground."
+  puts "Finishing with a small skip-step backward."
+end 
+def skip_step (left)
+  puts "Lower left foot to the ground."
+  puts "Bounce left foot back up slightly, kicking it a few inches back."
+  puts "Left, Right, Left and Left."
+  puts "Briefly"
+end
+def skip_step (right)
+  puts "Lower right foot to the ground."
+  puts "Bounce right foot back up slightly, kicking it a few inches back"
+  puts "Briefly"
+end 
+def bob_the_reins 
+  puts "Holding up your arms out in front of you, straight and at chest level."
+  puts "Cross your right wrist over your left and hold them together."
+  puts "Do"
+ 
+  
+  reins
+  reins
+  reins
+  reins
+  reins 
+  reins 
+  reins 
+  reins 
+end
+def reins
+  puts "Lift your arms up and down in a loose bouncing movement, in time with the beat of the song."
+end 
+def lasso 
+  puts "Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left."
+  puts "Lift right arm up and point right elbow diagonally to the right."
+  puts "Raise right forearm so that it points straight up, and whip it in small circles to the beat of the song, as if you were "
+  puts "Lift"
+end 
 
-# Code your methods below
+# def gangnam_style 
+# # Code your methods below
+# puts "Plant legs far apart, bend knees slightly and keep posture loose."
+# puts "Lift right foot."
+# puts " Return right foot to the ground."
+# puts "Finishing with a small skip-step backward."
+# puts "Lower left foot to the ground."
+# puts "Bounce right foot back up slightly, kicking it a few inches back."
+# puts "Right, Left, Right and Right."
+# puts "Holding your arms out in front of you, straight and at chest level."
+# puts "Cross your right wrist over your left and hold them together."
+# puts "Lift your arms up and down in a loose bouncing movement, in time with the beat of the song. This movement is repeated eight times."
+# puts "Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left."
+# puts "Lift right arm up and point right elbow diagonally to the right."
+# puts "Raise right forearm so that it points straight up, and whip it in small circles to the beat of the song, as if you were a cowboy holding a rope lasso. This movement is repeated eight times as well."
+# end 
+
+
